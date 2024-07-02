a37633110e13c917eff6383f0f3b89c432affe73
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-07-02 13:53:03 +0530

    deploy

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ import logo from './assets/products/online-shop.png'
 function Navbar() {
     return (
         <div className="navbar flex justify-between bg-black py-2 px-5">
-            <div className="logo w-16 ps-2"><img src={logo} alt="" /></div>
+            <div className="logo w-16 ps-2"><Link to={"/"}><img src={logo} alt="" /></Link></div>
             <div className="links">
                 <Link to={'/'}>Shop</Link>
                 <Link to={'/cart'}><ShoppingCart size={32} /></Link>
